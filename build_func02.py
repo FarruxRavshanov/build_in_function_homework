9ebab5d8c538a0ac742b712bf3f70469addac7ff
FarruxRavshanov <farruxlynx@gmail.com>
2022-10-05 12:25:52 +0300

    task02

--- build_func02.py
+++ build_func02.py
@@ -1,2 +1,6 @@
 # the task condition is placed in the README file
-#ttps://github.com/codeschool43/Build_in_function_homework#build_func02
+#https://github.com/codeschool43/Build_in_function_homework#build_func02
+
+n = 3 * ( 7 / 5 - 9 / 4 )
+answer = n
+print(n)
